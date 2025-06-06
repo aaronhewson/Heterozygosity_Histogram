77b3a3abaeda8c7cf20f1c2c4ef297308810908d
Aaron Hewson <aaronjhewson@gmail.com>
2025-06-06 16:54:04 +1200

    Initial commit

---
+++ Heterozygosity Histogram.R
@@ -0,0 +1,30 @@
+## This script produces a histogram of the heterozygosity by individual of the accessions from the JD and PFR collections that were SNP genotyped for this project
+## As per Larsen et al. 2018, this can be a diagnostic feature to distinguish diploid from triploid apples. 
+
+##Plot a histogram of the count of individual heterozygosity
+
+<<<<<<< HEAD
+#Set input directory for .txt file with heterozygosity information
+HetFile <- "C:/Users/curly/Desktop/Apple Genotyping/Results/Heterozygosity Histogram/Heterozygosity_Data.txt"
+
+#Set output directory for saving histogram as a PNG
+HetPNG <- "C:/Users/curly/Desktop/Apple Genotyping/Results/Heterozygosity Histogram/Heterozygosity_Count.png"
+
+#Read data
+data <- read.delim(HetFile, header=TRUE)
+het <- data$het_rate
+
+#Plot histogram and export as PNG
+png(filename = HetPNG, width = 1000, height = 600)
+hist(het, breaks = 25, xlim = c(15,45), main = "Heterozygosity by Individual", xlab = "Heterozygosity by Individual", ylab = "Count")
+=======
+#Read data
+data <- read.delim("Heterozygosity_Data.txt", header=TRUE)
+het <- data$het_rate
+
+#Plot histogram and export as PNG
+png(filename = "Heterozygosity_Count.png", width = 1000, height = 600)
+hist(het, breaks = 25, xlim = c(15,45), main = "Heterozygosity by Individual", xlab = "Total Heterozygosity by Individual", ylab = "Count")
+
+>>>>>>> b5b7b5d31a083a7f4f0bd9052d754bc4aac722b1
+dev.off()
