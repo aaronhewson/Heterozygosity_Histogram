46520e8f5f17a42cc9c3abab632a01bf5603568e
Aaron Hewson <aaronjhewson@gmail.com>
2025-07-21 14:21:54 +1200

    update font sizes

--- Heterozygosity Histogram.R
+++ Heterozygosity Histogram.R
@@ -14,8 +14,9 @@ data <- read.delim(HetFile, header=TRUE)
 het <- data$het_rate
 
 #Plot histogram and export as PNG
-par(mar = c(0,150,0,0))
-png(filename = HetPNG, width = 1200, height = 800)
-hist(het, breaks = 25, xlim = c(15,45), main = "Heterozygosity by Individual", xlab = "Total heterozygosity by individual", ylab = "Count", cex.main=2,cex.lab=2,cex.axis=1.5)
+
+png(filename = HetPNG, width = 1000, height = 800)
+par(mar = c(7,5,5,1))
+hist(het, breaks = 30, xlim = c(0.15,0.45), main = "Heterozygosity by Individual", xlab = "Total heterozygosity by individual", ylab = "Count", cex.main=2,cex.lab= 1.85,cex.axis=1.75, mgp = c(3,1,0))
 
 dev.off()
